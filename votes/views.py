b1906d711822e9215ed98d9be16064024bf69e99
David Wyde <david.wyde@gmail.com>
2014-01-13 21:19:12 -0600

    Fix a race condition.

--- votes/views.py
+++ votes/views.py
@@ -9,10 +9,10 @@ def results(request):
 
 def vote(request):
     if request.method == 'POST':
-        team = Team.objects.get(pk=request.POST['team'])
-        team.votes = F('votes') + 1
-        team.save()
-        return render(request, 'done_voting.html', {'team': team})
+        team_id = request.POST.get('team')
+        if team_id is not None:
+            Team.objects.filter(pk=team_id).update(votes=F('votes') + 1)
+        return render(request, 'done_voting.html', {'team': team_id})
     else:
         teams = Team.objects.all()
         return render(request, 'voting.html', {'teams': teams})
